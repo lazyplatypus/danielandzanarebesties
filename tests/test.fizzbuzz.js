55eda309fbecccda77a2ded37e96a667d2162ff6
Daniel Kim <dgkim@ucdavis.edu>
2021-02-24 10:34:52 -0800

    Update test.fizzbuzz.js

--- tests/test.fizzbuzz.js
+++ tests/test.fizzbuzz.js
@@ -5,4 +5,12 @@ catch(e) {
     throw new Error("FIZZBUZ AINT FIZZIN' - DANIEL");
 }
 
-console.log("we're fizzin")
+let fizzbuzzing = fizzbuzz(10)
+let test_output = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
+
+if(fizzbuzzing != test_output){
+    throw new Error(`You're not fizzing well - Zan - Got: ${fizzbuzzing}, was expecting: ${test_output}.`)
+}
+
+console.log("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
+
