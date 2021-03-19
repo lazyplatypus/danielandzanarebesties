8099ebd7df8c55bb210430ec8349cb8b9c08efd7
Daniel Kim <dgkim@ucdavis.edu>
2021-03-19 10:41:07 -0700

    Update test.fizzbuzz.js

--- tests/test.fizzbuzz.js
+++ tests/test.fizzbuzz.js
@@ -13,10 +13,18 @@ if (fizzbuzzing.length == test_output.length
         return u === test_output[i];
     })
 ) {
+    let payloadSuccess = [{
+        "eventType":"Commit",
+        "pass":true,
+    }];
+    process.env.PAYLOAD = JSON.stringify(payloadSuccess);
     console.log("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
 }
 else {
+    let payloadFail = [{
+        "eventType":"Commit",
+        "pass":false,
+    }];
+    process.env.PAYLOAD = JSON.stringify(payloadFail);
     throw new Error(`You're not fizzing well - Zan - Got: ${fizzbuzzing}, was expecting: ${test_output}.`)
 }
-
-
